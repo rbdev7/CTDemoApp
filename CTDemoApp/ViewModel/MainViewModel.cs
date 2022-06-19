2ccbfc9bf8c78b31c5a78ba1cf48dad66bb12d85
rbdev7 <roy.burgess7@outlook.com>
2022-06-19 13:18:41 +0100

    Created MainViewModel class and added components and bindings to MainWindow view.

---
+++ CTDemoApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CTDemoApp.ViewModel
+{
+    // ViewModel classes using the MVVM Toolkit need to be declared as partial due to the MVVM toolkit generating partial code for the class.
+    public partial class MainViewModel : ObservableObject
+    {
+        // The ObservableProperty attribute generates getters and setters based on instance variable names.
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        string _firstName = "";
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        string _lastName = "";
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        [ICommand]
+        void Submit()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
